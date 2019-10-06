1698815fcf24749ed0308ab7c3248144ecb522af
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-10-05 21:00:04 -0700

    data showing on employeeid page

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/';
+
+var str = "";
+
+app.route('/employeeid').get(function(req, res){
+  MongoClient.connect(url, function(err, client){
+    var db = client.db('EmployeesDB');
+    var cursor = db.collection('Employee').find();
+
+
+    cursor.each(function(err, item){
+      // console.log(item)
+      if(item != null){
+        str = str + "&nbsp;&nbsp;&nbsp;&nbsp;Employee id&nbsp;&nbsp;" + item.Employeeid + "</br>";
+      }
+    });
+
+    res.send(str);
+    client.close()
+  })
+});
+
 app.route('/Node').get(function(req, res)
 {
   res.send("Tutorial on Node");
